131966e0275b0f06fc8e6ba4620056944e2bb8c9
Pauline Lago <lagopauline28@gmail.com>
2025-10-02 11:58:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6877234553-fd6c1",
+  "appId": "1:240708842001:web:972119865a465edb0377b0",
+  "apiKey": "AIzaSyAywZENY1LcW6Skt5W24DCuUKlNZfCQ7EM",
+  "authDomain": "studio-6877234553-fd6c1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "240708842001"
+};
